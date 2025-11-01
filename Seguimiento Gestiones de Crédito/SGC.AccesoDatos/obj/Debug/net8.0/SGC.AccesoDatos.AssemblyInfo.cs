44aa881053a611bc04b78a2cec67186bb0ebe5a0
Brandon-0717 <brandon0717.cr@gmail.com>
2025-10-31 22:00:34 -0600

    adicion de archivos para mantener carpetas

--- Seguimiento Gestiones de Crédito/SGC.AccesoDatos/obj/Debug/net8.0/SGC.AccesoDatos.AssemblyInfo.cs
+++ Seguimiento Gestiones de Crédito/SGC.AccesoDatos/obj/Debug/net8.0/SGC.AccesoDatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGC.AccesoDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f920a54d9b411bc4e1a14d02e109769db5edbfee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38d0333db27ae5d64d83698b0522409ff4ef1ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGC.AccesoDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGC.AccesoDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
